0ad4e83a0bc40dfe6949cb2312e370b9bba63423
RitikaRege31 <138657543+RitikaRege31@users.noreply.github.com>
2024-07-22 16:09:56 +0530

    commits

--- pages/[id].js
+++ pages/[id].js
@@ -1,24 +1,24 @@
-// pages/[id].js
-import React from 'react';
-import { fetchCoinDetail } from '../lib/api';
-import CoinDetail from '../components/CoinDetails';
-import styles from '../styles/CoinDetail.module.css';
+// // pages/[id].js
+// import React from 'react';
+// import { fetchCoinDetail } from '../lib/api';
+// import CoinDetail from '../components/CoinDetails';
+// import styles from '../styles/CoinDetail.module.css';
 
-const CoinPage = ({ coin }) => {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <CoinDetail coin={coin} />
-      </main>
-    </div>
-  );
-};
+// const CoinPage = ({ coin }) => {
+//   return (
+//     <div className={styles.container}>
+//       <main className={styles.main}>
+//         <CoinDetail coin={coin} />
+//       </main>
+//     </div>
+//   );
+// };
 
-export async function getServerSideProps({ params }) {
-  const coin = await fetchCoinDetail(params.id);
-  return {
-    props: { coin },
-  };
-}
+// export async function getServerSideProps({ params }) {
+//   const coin = await fetchCoinDetail(params.id);
+//   return {
+//     props: { coin },
+//   };
+// }
 
-export default CoinPage;
+// export default CoinPage;
